875ce0b627fce70dbe84646f409bcbce602964d6
Alexander Henderson <a_henderson@alexsguardian.net>
2024-06-06 14:26:48 -0400

    Site v1 (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,23 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }), 
+  compress(),
+  partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),
+  ],
 })
